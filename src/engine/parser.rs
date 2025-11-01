b1e1d5a724a8fa2b85cee6f11df946c605a77855
Crimcode <valentin@vimunteanu.me>
2025-11-01 15:11:31 +0200

    feat: Add function parsing
    Add function support and sum, avg functions

--- src/engine/parser.rs
+++ src/engine/parser.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::{collections::HashMap, fmt};
 
 use pest::{iterators::Pairs, pratt_parser::PrattParser, Parser};
 use pest_derive::Parser;
@@ -7,6 +7,7 @@ use pest_derive::Parser;
 pub enum FormulaError {
     ParsingError,
     DivBy0,
+    UnknownFunction,
 }
 
 impl fmt::Display for FormulaError {
@@ -14,6 +15,7 @@ impl fmt::Display for FormulaError {
         match self {
             FormulaError::ParsingError => write!(f, "#NAME?"),
             FormulaError::DivBy0 => write!(f, "#DIV/0!"),
+            FormulaError::UnknownFunction => write!(f, "#NAME?"),
         }
     }
 }
@@ -21,6 +23,8 @@ impl fmt::Display for FormulaError {
 #[derive(Parser)]
 #[grammar = "engine/cell_formula.pest"]
 struct CellFormulaParser;
+
+type SpreadsheetFunction = fn(&[f64]) -> Result<f64, FormulaError>;
 
 lazy_static::lazy_static! {
     static ref PRATT_PARSER: PrattParser<Rule> = {
@@ -32,13 +36,30 @@ lazy_static::lazy_static! {
             .op(Op::infix(pow, Right))
             .op(Op::prefix(neg))
     };
+    static ref FUNCTION_REGISTRY: HashMap<&'static str, SpreadsheetFunction> = {
+        let mut m = HashMap::new();
+        m.insert("SUM", sum as SpreadsheetFunction);
+        m.insert("AVG", avg as SpreadsheetFunction);
+        m
+    };
+}
+
+fn sum(args: &[f64]) -> Result<f64, FormulaError> {
+    Ok(args.iter().sum())
+}
+
+fn avg(args: &[f64]) -> Result<f64, FormulaError> {
+    if args.is_empty() {
+        return Err(FormulaError::DivBy0);
+    }
+    Ok(args.iter().sum::<f64>() / args.len() as f64)
 }
 
 #[derive(Debug)]
 enum Expr {
     Number(f64),
     CellRef(String),
-
+    // Range(String, String),
     BinaryOp {
         op: BinOp,
         lhs: Box<Expr>,
@@ -48,6 +69,11 @@ enum Expr {
     UnaryOp {
         op: UnOp,
         operand: Box<Expr>,
+    },
+
+    Function {
+        name: String,
+        args: Vec<Expr>,
     },
 }
 
@@ -70,6 +96,18 @@ fn parse_expr(pairs: Pairs<Rule>) -> Expr {
         .map_primary(|primary| match primary.as_rule() {
             Rule::number => Expr::Number(primary.as_str().parse().unwrap()),
             Rule::cell_ref => Expr::CellRef(primary.as_str().to_string()),
+            Rule::func => {
+                let mut inner = primary.into_inner();
+                let name = inner.next().unwrap().as_str().to_string();
+                let Some(args) = inner.next() else {
+                    return Expr::Function { name, args: vec![] };
+                };
+                let args: Vec<Expr> = args
+                    .into_inner()
+                    .map(|arg| parse_expr(arg.into_inner()))
+                    .collect();
+                Expr::Function { name, args }
+            }
             Rule::expr => parse_expr(primary.into_inner()),
             rule => unreachable!("Expr::parse expected atom, found {:?}", rule),
         })
@@ -111,6 +149,7 @@ fn eval_expr(
             Some(value) => Ok(value),
             None => Ok(0.0),
         },
+        // Expr::Range(_c1, _c2) => unimplemented!(),
         Expr::BinaryOp { op, lhs, rhs } => {
             let lval = self::eval_expr(lhs, cell_ref_resolver)?;
             let rval = self::eval_expr(rhs, cell_ref_resolver)?;
@@ -119,6 +158,17 @@ fn eval_expr(
         Expr::UnaryOp { op, operand } => {
             let val = eval_expr(operand, cell_ref_resolver)?;
             Ok(eval_unary_op(op, val))
+        }
+        Expr::Function { name, args } => {
+            let args = args
+                .iter()
+                .map(|arg| eval_expr(arg, cell_ref_resolver))
+                .collect::<Result<Vec<f64>, FormulaError>>()?;
+            let func = FUNCTION_REGISTRY
+                .get(name.to_uppercase().as_str())
+                .ok_or(FormulaError::UnknownFunction)?;
+
+            func(&args)
         }
     }
 }
@@ -170,6 +220,9 @@ mod tests {
         assert!(CellFormulaParser::parse(Rule::formula, "= 3 + -12 / 9").is_ok());
         assert!(CellFormulaParser::parse(Rule::formula, "= a1 + -12 / 9").is_ok());
         assert!(CellFormulaParser::parse(Rule::formula, "= a1 + -B2 / 9").is_ok());
+        assert!(CellFormulaParser::parse(Rule::formula, "= SUM(1,a1,-3)").is_ok());
+        assert!(CellFormulaParser::parse(Rule::formula, "= sum()").is_ok());
+        assert!(CellFormulaParser::parse(Rule::formula, "= a1:b3").is_err());
     }
 
     fn mock_cell_ref_resolver(cell_ref: &str) -> Option<f64> {
@@ -196,5 +249,21 @@ mod tests {
             calculate("= -a1 + B2 * 2", &mock_cell_ref_resolver),
             Ok(3.0)
         );
+    }
+
+    #[test]
+    fn calculate_functions() {
+        assert_eq!(calculate("=Sum(1,2,3)", &mock_cell_ref_resolver), Ok(6.0));
+        assert_eq!(calculate("=avG(1,2,3)", &mock_cell_ref_resolver), Ok(2.0));
+        assert_eq!(
+            calculate("=avG()", &mock_cell_ref_resolver),
+            Err(FormulaError::DivBy0)
+        );
+        assert_eq!(
+            calculate("=Sum(a1, b2 * 3)", &mock_cell_ref_resolver),
+            Ok(7.0)
+        );
+        assert_eq!(calculate("=avG(a1,b2,3)", &mock_cell_ref_resolver), Ok(2.0));
+        assert_eq!(calculate("=sum()", &mock_cell_ref_resolver), Ok(0.0));
     }
 }
